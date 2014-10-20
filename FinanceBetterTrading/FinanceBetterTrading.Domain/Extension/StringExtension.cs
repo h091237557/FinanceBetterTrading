f172704e5d5cb22c02966c9f1cc6c663b0fa8481
Lin <h091237557@gmail.com>
2014-10-20 12:48:54 +0800

    brab1test1

---
+++ FinanceBetterTrading/FinanceBetterTrading.Domain/Extension/StringExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceBetterTrading.Domain.Extension
+{
+    public static class StringExtension
+    {
+        public static int ParseThousandthString(this string thousandthStr)
+        {
+            int _value = -1;
+            if (!string.IsNullOrEmpty(thousandthStr))
+            {
+                try
+                {
+                    _value = int.Parse(thousandthStr, NumberStyles.AllowThousands | NumberStyles.AllowDecimalPoint | NumberStyles.AllowLeadingSign);
+                }
+                catch (Exception ex)
+                {
+                    _value = -1;
+                }
+            }
+            return _value;
+        }
+    }
+}
