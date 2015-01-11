c618d310e3cc853314a44633bd8237d0114d48a2
jack5241027 <jack5241027@gmail.com>
2015-01-11 11:27:42 +0800

    Revert "將SpectFlower導入，並撰寫『依照股票Code來尋找出該支股票』的測試"
    This reverts commit e18826fa7315cd673417ea522a439fee9daace2f.

--- FinanceBetterTrading/FinanceBetterTrading.Test/WebRequestTests/RequstServerTest.cs
+++ FinanceBetterTrading/FinanceBetterTrading.Test/WebRequestTests/RequstServerTest.cs
@@ -20,5 +20,41 @@ namespace FinanceBetterTrading.UITest.WebRequestTests
             var result = requestTwse.GetStockPrice("3514");
             Assert.IsNotNull(result);
         }
+
+        [TestMethod]
+        public void TestGetStockPriceIsNull()
+        {
+            RequestTWSE requestTwse = new RequestTWSE();
+            var result = requestTwse.GetStockPrice("0000");
+
+            var expect = 0;
+            var actual = result.Count;
+            Assert.AreEqual(expect,actual);
+        }
+
+        [TestMethod]
+        public void TestDate()
+        {
+            string date = "101/2/29";
+            RequestTWSE requestTwse = new RequestTWSE();
+            requestTwse.ChangeDateFormate(date);
+            string test = "";
+            //DateTime time = Convert.ToDateTime(date);
+        }
+
+        [TestMethod]
+        public void TestPostdata()
+        {
+            RequestServer requestServer = new RequestServer();
+            StringBuilder postData = new StringBuilder();
+            postData.Append(HttpUtility.UrlEncode(String.Format("report1={0}&", "day")));
+            postData.Append(HttpUtility.UrlEncode(String.Format("input_date={0}&", "103/12/05")));
+            postData.Append(String.Format("mSubmit={0}&", "%ACd%B8%DF"));
+            postData.Append(HttpUtility.UrlEncode(String.Format("yr={0}", "2014")));
+            postData.Append(HttpUtility.UrlEncode(String.Format("w_date={0}&", "20141201")));
+            postData.Append(HttpUtility.UrlEncode(String.Format("m_date={0}", "20141201")));
+            string url = "http://www.twse.com.tw/ch/trading/fund/BFI82U/BFI82U.php";
+            var result = requestServer.GetPostHtmlData(url, postData.ToString(), "/html[1]");
+        }
     }
 }
